8a296ac6ffb2092e41f184500a59192360b9e459
netkawai <netkawai@gmail.com>
2022-09-15 11:29:25 -0400

    Add more templates

--- WpfAppTemplatePractice/WpfAppTemplatePractice/Window1.xaml.cs
+++ WpfAppTemplatePractice/WpfAppTemplatePractice/Window1.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +16,32 @@ using System.Windows.Shapes;
 
 namespace WpfAppTemplatePractice
 {
+
+    public class TestViewListItem : INotifyPropertyChanged
+    {
+        public Color TextColor { get; set; }
+        public bool IsChecked { get; set; } = true;
+        public string Label { get; set; }
+        public string Note { get; set; }
+        public object Tag { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
     public partial class Window1 : Window
     {
+        ObservableCollection<TestViewListItem> Items = new();
+
         public Window1()
         {
+            Items.Add(new TestViewListItem() { Label = "test", Note = "test note" });
+            Items.Add(new TestViewListItem() { Label = "test2", Note = "test2 note" });
+
             InitializeComponent();
+            DataContext = this;
         }
         private void Expander_Collapsed(object sender, RoutedEventArgs e)
         {
