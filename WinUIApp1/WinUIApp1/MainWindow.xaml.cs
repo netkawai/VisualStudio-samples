b2dfa3a7cab5f45eb376e7ae0aebdf0e59195ddf
netkawai <netkawai@gmail.com>
2022-09-20 10:32:55 -0400

    Removed AppHost(obsolete) and moved MSGraph sample(to cross platform)

--- WinUIApp1/WinUIApp1/MainWindow.xaml.cs
+++ WinUIApp1/WinUIApp1/MainWindow.xaml.cs
@@ -7,11 +7,17 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using WinRT.Interop;
+using WinRT;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,8 +34,47 @@ namespace WinUIApp1
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
+        internal interface IInitializeWithWindow
         {
+            void Initialize(IntPtr hwnd);
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+        internal interface IWindowNative
+        {
+            IntPtr WindowHandle { get; }
+        }
+
+        private async Task ShowDialog(string msg)
+        {
+            var dlg = new MessageDialog(msg);
+            IInitializeWithWindow withWindow = dlg.As<IInitializeWithWindow>();
+            var handle = this.As<IWindowNative>().WindowHandle;
+            try
+            {
+                withWindow.Initialize(handle);
+                await dlg.ShowAsync();
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private async void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            var thisWindowQueue =  this.DispatcherQueue;
+
+            if (Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread() == thisWindowQueue)
+                await ShowDialog("Equal");
+            else
+                await ShowDialog("Not equal");
+
             myButton.Content = "Clicked";
         }
     }
